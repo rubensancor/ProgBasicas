3043378e0151e61cc0ad97075adc8277fa223bed
rubensancor <ruben.sanchez@deusto.es>
2022-09-21 13:50:12 +0200

    Production script

--- start-prod.sh
+++ start-prod.sh
@@ -1,5 +1,13 @@
 #! /usr/bin/env bash
 
 cd app
+
+# Create admin if not created
+python manage.py createsuperuser --no-input
+# Set database
+python manage.py migrate
+python manage.py loaddata initial_data.json
+# Collect statics
 python manage.py collectstatic --noinput
+
 gunicorn progbasicas.wsgi --bind 0.0.0.0:8000 --forwarded-allow-ips="*"
